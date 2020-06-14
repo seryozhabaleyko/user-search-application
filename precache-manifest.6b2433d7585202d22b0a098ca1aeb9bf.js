15aae4e3964dd4d92d7be59e0c8e4acfcef8deae
seryozhabaleyko <seryozha.baleyko@gmail.com>
2020-06-15 00:08:32 +0300

    Updates

---
+++ precache-manifest.6b2433d7585202d22b0a098ca1aeb9bf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3e4720fb7e33203c242bcebb0691df2",
+    "url": "/github-user-search-application/index.html"
+  },
+  {
+    "revision": "06097a15f6e3453f6fce",
+    "url": "/github-user-search-application/static/css/2.7f9cf1a0.chunk.css"
+  },
+  {
+    "revision": "33a2dd18ee87edfe8af2",
+    "url": "/github-user-search-application/static/css/main.7625edd3.chunk.css"
+  },
+  {
+    "revision": "06097a15f6e3453f6fce",
+    "url": "/github-user-search-application/static/js/2.9d0f1176.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/github-user-search-application/static/js/2.9d0f1176.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33a2dd18ee87edfe8af2",
+    "url": "/github-user-search-application/static/js/main.9fa557b8.chunk.js"
+  },
+  {
+    "revision": "1383522dbd84839fafce",
+    "url": "/github-user-search-application/static/js/runtime-main.ba8b55a0.js"
+  }
+]);
